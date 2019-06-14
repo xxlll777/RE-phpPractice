1bf5e3ffac62efc338697514712918809aa2d54d
taira <xxnagomixx777@gmail.com>
2019-06-14 22:42:14 +0900

    userLogin画面作成

---
+++ join/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>userLogin</title>
+</head>
+<body>
+	
+
+	<form action="" method="post" enctype="application/x-www-form-urlencoded"></form>
+<dl>
+	<dt>名前</dt>
+	<dd><input type="text" name="name"></dd>
+	<dt>メールアドレス</dt>
+	<dd><input type="text" name="mail"></dd>
+	<dt>パスワード</dt>
+	<dd><input type="password" name="password"></dd>
+	<dt>写真のアップロード</dt>
+	<dd><input type="file" name="image"></dd>
+</dl>
+
+<input type="submit" value="確認">
+
+
+</body>
+</html>
